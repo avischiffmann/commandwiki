e8a3d86a37dc33e98754ddf280e17e0d1f7484e7
Avi Schiffmann <57499070+avischiffmann@users.noreply.github.com>
2020-09-06 06:48:49 -0700

    Add files via upload

---
+++ search.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import wikipedia
+import argparse
+
+def wiki(txt):
+    txt = wikipedia.page(txt)
+    print(txt.summary)
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("txt", help="What do you want to search?", type=str)
+
+    args = parser.parse_args()
+
+    wiki(args.txt)
+
+if __name__ == "__main__":
+    Main()
---
+++ wiki.bat
@@ -0,0 +1,3 @@
+@echo off
+python3 c:\Users\avisc\Documents\commandwiki\search.py %1
+pause
